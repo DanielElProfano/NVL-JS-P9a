3a34640e026b49fa1a60c9130a34b13faec4649a
Daniel Gonzalez <daniel.gonzalez.guijarro@gmail.com>
2020-08-05 20:19:33 +0200

    PRACTICA CON MATRICES

---
+++ NVL-JS-P9a.js
@@ -0,0 +1,10 @@
+var antonimos =[["alto","bajo"],["primero","ultimo"],["claro","oscuro"]];
+//Indica la longitud del array
+alert(antonimos.length);
+//Indica la longitud de uno de los elementos del array
+alert(antonimos[1].length);
+/*Muestra por pantalla el valor del tercer elemento de la lista del array.
+ Y dentro de ese tercer elemento, su segundo
+  elemento.*/
+alert(antonimos[3]);
+alert(antonimos[3][1]);
